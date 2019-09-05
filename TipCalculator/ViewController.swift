e16bef7b799cc1fd37559fa6b388b407d9e4991e
Antonio Vega <antoniovegajr@Antonios-MacBook-Pro.local>
2019-09-04 18:46:14 -0700

    initial commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Antonio Vega Jr on 8/28/19.
+//  Copyright © 2019 Antonio Vega Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var percentControl: UISegmentedControl!
+    @IBOutlet weak var inputField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercent = [0.50,0.75]
+        let bill = Double(inputField.text!) ?? 0
+        let tip = (bill * tipPercent[percentControl.selectedSegmentIndex])
+        let total = tip + bill
+        tipLabel.text = String(format: "$%.2f",tip)
+        totalLabel.text = String(format: "$%.2f",total)
+    }
+    
+    
+}
+
